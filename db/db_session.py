58854c8bb9b237a2a7d2d5424c20aeacf6693486
Artem <artem00561@gmail.com>
2023-03-16 21:15:17 +0300

    init: base sqlalchemy
    Took 8 minutes

---
+++ db/db_session.py
@@ -0,0 +1,40 @@
+import os
+
+import sqlalchemy as sa
+import sqlalchemy.ext.declarative as dec
+import sqlalchemy.orm as orm
+from sqlalchemy.orm import Session
+
+SqlAlchemyBase = dec.declarative_base()
+
+__factory = None
+
+
+def global_init():
+    global __factory
+
+    if __factory:
+        return
+
+    db_engine = os.getenv("DB_ENGINE", "sqlite")
+    if db_engine == "postgresql":
+        db_connection = f"postgresql://{os.getenv('POSTGRESQL_USERNAME')}:{os.getenv('POSTGRESQL_PASSWORD')}@{os.getenv('POSTGRESQL_HOST')}:{os.getenv('POSTGRESQL_PORT')}/{os.getenv('POSTGRESQL_DB_NAME')}"
+    elif db_engine == "sqlite":
+        db_dir = os.getenv("SQLITE_DIR", "db/database.db")
+        db_connection = f"sqlite:///{db_dir.strip()}?check_same_thread=False"
+    else:
+        raise Exception("Неподдерживаемый тип базы данных.")
+
+    print(f"Подключение к базе данных по адресу {db_connection}")
+
+    engine = sa.create_engine(db_connection, echo=False)
+    __factory = orm.sessionmaker(bind=engine)
+
+    # from . import __all_models
+
+    SqlAlchemyBase.metadata.create_all(engine)
+
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from flask import Flask
 
+from db import db_session
+
 # from routes import routes
-from settings import settings
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = settings.SECRET_KEY
-app.config["SQLALCHEMY_DATABASE_URI"] = settings.DB_URL
 
-db = SQLAlchemy(app)
 
-with app.app_context():
-    db.create_all()
-# app.register_blueprint(routes.blueprint)
+def main():
+    db_session.global_init()
+    app.run(port=8080, host="0.0.0.0")
 
 
 if __name__ == "__main__":
-    app.run(port=8080, host="0.0.0.0")
+    main()

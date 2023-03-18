9f2401427cce6a312ef56c967cc68dc2141f60b3
Deformater <muravenkog@yandex.ru>
2023-03-18 22:52:16 +0300

    feat: add shemas

--- routes/routes.py
+++ routes/routes.py
@@ -1,4 +1,5 @@
 import flask
+from db.db_session import create_session
 
 
 blueprint = flask.Blueprint("preprof_routes", __name__, template_folder="templates")
@@ -6,5 +7,13 @@ blueprint = flask.Blueprint("preprof_routes", __name__, template_folder="templat
 
 @blueprint.route("/", methods=["GET"])
 def home_page():
+    db_sess = create_session()
+
     return "aboba"
 
+
+@blueprint.route("/profile", methods=["GET", "POST"])
+def profile_page():
+    session = create_session()
+
+    return "aboba"
